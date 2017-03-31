6719cda43cb8b9c213d168b3d5d4cd561194e23a
Wilson Carey <carey@unif.io>
2017-03-31 10:07:33 -0400

    Updated Base module

--- base/outputs.tf
+++ base/outputs.tf
@@ -1,21 +1,17 @@
 # Output Variables
 
-## Returns ID of the VPC
-output "vpc_id" {
-  value = "${aws_vpc.vpc.id}"
+output "flow_log_id" {
+  value = "${aws_flow_log.flow_log.id}"
 }
 
-## Returns ID of the Internet gateway
 output "igw_id" {
   value = "${aws_internet_gateway.igw.id}"
 }
 
-## Returns ID of the DMZ routing table
 output "rt_dmz_id" {
-  value = "${module.rt_dmz.rt_id}"
+  value = "${aws_route_table.rt_dmz.id}"
 }
 
-## Returns ID of the VPC flow log
-output "flow_log_id" {
-  value = "${aws_flow_log.flow_log.id}"
+output "vpc_id" {
+  value = "${aws_vpc.vpc.id}"
 }

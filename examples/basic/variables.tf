f5c8e21c8b6dbd026e7333cd3fbba04ae3e7f57c
Wilson Carey <carey@unif.io>
2017-03-31 10:11:06 -0400

    Updated example & tests

--- examples/basic/variables.tf
+++ examples/basic/variables.tf
@@ -1,64 +1,56 @@
 # Input Variables
 
-## Resource Tags
-variable "stack_item_label" {
-  type        = "string"
-  description = "Short form identifier for this stack. This value is used to create the 'Name' resource tag for resources created by this stack item, and also serves as a unique key for re-use."
-}
-
+## Resource tags
 variable "stack_item_fullname" {
-  type        = "string"
-  description = "Long form descriptive name for this stack item. This value is used to create the 'application' resource tag for resources created by this stack item."
+  type = "string"
 }
 
-## VPC Parameters
-variable "region" {
-  type        = "string"
-  description = "The AWS region"
-  default     = "us-west-2"
+variable "stack_item_label" {
+  type = "string"
 }
 
-variable "vpc_cidr" {
-  type        = "string"
-  description = "The CIDR block you want the VPC to cover. For example: 10.0.0.0/16"
-  default     = "10.10.0.0/22"
-}
-
-variable "az" {
-  type        = "map"
-  description = "The AWS availability zone"
-
-  default = {
-    us-west-2 = "a,b,c"
-  }
-}
-
-variable "lans_per_az" {
-  type        = "string"
-  description = "The number of private LAN subnets to be provisioned per AZ"
-  default     = 1
-}
-
-variable "enable_dmz_public_ips" {
-  type        = "string"
-  description = "Specify true to indicate that instances launched into the DMZ subnet should be assigned a public IP address"
-  default     = false
-}
-
+## VPC base parameters
 variable "enable_dns" {
-  type        = "string"
-  description = "Specifies whether DNS resolution is supported for the VPC"
-  default     = false
+  type    = "string"
+  default = ""
 }
 
 variable "enable_hostnames" {
-  type        = "string"
-  description = "Specifies whether the instances launched in the VPC get DNS hostnames"
-  default     = false
+  type    = "string"
+  default = ""
 }
 
-variable "vpg_vpc_attach" {
-  type        = "string"
-  description = "Flag for selecting whether the VPG should be attached to a VPC."
-  default     = "0"
+variable "region" {
+  type = "string"
+}
+
+variable "vpc_cidr" {
+  type = "string"
+}
+
+## AZ parameters
+variable "azs_provisioned" {
+  type    = "string"
+  default = ""
+}
+
+variable "enable_dmz_public_ips" {
+  type    = "string"
+  default = ""
+}
+
+variable "lans_per_az" {
+  type    = "string"
+  default = "1"
+}
+
+variable "nat_eips_enabled" {
+  type    = "string"
+  default = ""
+}
+
+## VPG parameters
+variable "vpc_attach" {
+  type    = "string"
+  default = ""
 }
---
+++ examples/complete/main.tf
@@ -0,0 +1,122 @@
+# AWS Virtual Private Cloud
+
+## Configures AWS provider
+provider "aws" {
+  region = "${var.region}"
+}
+
+## Configures base VPC
+module "vpc_base" {
+  # Example GitHub source
+  #source = "github.com/unifio/terraform-aws-vpc?ref=master//base"
+  source = "../../base"
+
+  enable_classiclink  = "${var.enable_classiclink}"
+  enable_hostnames    = "${var.enable_hostnames}"
+  instance_tenancy    = "${var.instance_tenancy}"
+  stack_item_fullname = "${var.stack_item_fullname}"
+  stack_item_label    = "${var.stack_item_label}"
+  vpc_cidr            = "${var.vpc_cidr}"
+}
+
+## Configures DHCP
+module "vpc_dhcp" {
+  # Example GitHub source
+  #source = "github.com/unifio/terraform-aws-vpc?ref=master//dhcp"
+  source = "../../dhcp"
+
+  domain_name          = "${var.domain_name}"
+  name_servers         = ["${split(",",var.name_servers)}"]
+  netbios_name_servers = ["${split(",",var.netbios_name_servers)}"]
+  netbios_node_type    = "${var.netbios_node_type}"
+  ntp_servers          = ["${split(",",var.ntp_servers)}"]
+  stack_item_fullname  = "${var.stack_item_fullname}"
+  stack_item_label     = "${var.stack_item_label}"
+  vpc_id               = "${module.vpc_base.vpc_id}"
+}
+
+## Configures ACLs
+resource "aws_network_acl" "acl" {
+  vpc_id     = "${module.vpc_base.vpc_id}"
+  subnet_ids = ["${concat(split(",",module.vpc_az.lan_ids),split(",",module.vpc_az.dmz_ids))}"]
+
+  tags {
+    application = "${var.stack_item_fullname}"
+    managed_by  = "terraform"
+    Name        = "${var.stack_item_label}-acl"
+  }
+}
+
+## Configures Virtual Private Gateway
+module "vpc_vpg" {
+  # Example GitHub source
+  #source = "github.com/unifio/terraform-aws-vpc?ref=master//vpg"
+  source = "../../vpg"
+
+  vpc_attach          = "true"
+  vpc_id              = "${module.vpc_base.vpc_id}"
+  stack_item_fullname = "${var.stack_item_fullname}"
+  stack_item_label    = "${var.stack_item_label}"
+}
+
+module "vpc_az" {
+  # Example GitHub source
+  #source = "github.com/unifio/terraform-aws-vpc?ref=master//az"
+  source = "../../az"
+
+  azs_provisioned_override = "${var.azs_provisioned_override}"
+
+  dmz_cidrs = ["${cidrsubnet(var.vpc_cidr,3,0)}",
+    "${cidrsubnet(var.vpc_cidr,3,1)}",
+    "${cidrsubnet(var.vpc_cidr,3,2)}",
+    "${cidrsubnet(var.vpc_cidr,3,3)}",
+  ]
+
+  lan_cidrs = ["${cidrsubnet(var.vpc_cidr,4,8)}",
+    "${cidrsubnet(var.vpc_cidr,4,9)}",
+    "${cidrsubnet(var.vpc_cidr,4,10)}",
+    "${cidrsubnet(var.vpc_cidr,4,11)}",
+    "${cidrsubnet(var.vpc_cidr,4,12)}",
+    "${cidrsubnet(var.vpc_cidr,4,13)}",
+    "${cidrsubnet(var.vpc_cidr,4,14)}",
+    "${cidrsubnet(var.vpc_cidr,4,15)}",
+  ]
+
+  lans_per_az          = "${var.lans_per_az}"
+  nat_eips_enabled     = "false"
+  nat_gateways_enabled = "${var.nat_gateways_enabled}"
+  rt_dmz_id            = "${module.vpc_base.rt_dmz_id}"
+  stack_item_label     = "${var.stack_item_label}"
+  stack_item_fullname  = "${var.stack_item_fullname}"
+  vgw_ids              = ["${module.vpc_vpg.vpg_id}"]
+  vpc_id               = "${module.vpc_base.vpc_id}"
+}
+
+## Configures routing
+resource "aws_route" "dmz-to-igw" {
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${module.vpc_base.igw_id}"
+  route_table_id         = "${module.vpc_base.rt_dmz_id}"
+}
+
+resource "aws_route" "lan-to-nat-gw" {
+  count = "${length(var.azs_provisioned_override) * (length(var.lans_per_az) > 0 ? var.lans_per_az : "1") * signum(var.nat_gateways_enabled == "true" ? "1" : "0")}"
+
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = "${element(split(",",module.vpc_az.nat_ids),count.index)}"
+  route_table_id         = "${element(split(",",module.vpc_az.rt_lan_ids),count.index)}"
+}
+
+resource "aws_route" "lan-to-nat" {
+  count = "${length(var.azs_provisioned_override) * (length(var.lans_per_az) > 0 ? var.lans_per_az : "1") * signum(var.nat_gateways_enabled == "true" ? "0" : "1")}"
+
+  destination_cidr_block = "0.0.0.0/0"
+  instance_id            = "${element(split(",",module.vpc_az.nat_ids),count.index)}"
+  route_table_id         = "${element(split(",",module.vpc_az.rt_lan_ids),count.index)}"
+}
+
+resource "aws_vpc_endpoint" "s3-ep" {
+  route_table_ids = ["${split(",",module.vpc_az.rt_lan_ids)}"]
+  service_name    = "com.amazonaws.${var.region}.s3"
+  vpc_id          = "${module.vpc_base.vpc_id}"
+}

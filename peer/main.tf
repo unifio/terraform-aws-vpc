f17228a110227c1c23b5dc4ff397316bce792713
Wilson Carey <carey@unif.io>
2017-04-03 13:02:23 -0400

    Updated Peer module

--- peer/main.tf
+++ peer/main.tf
@@ -1,45 +1,59 @@
 # Peering Connection
 
+## Set Terraform version constraint
+terraform {
+  required_version = "> 0.8.0"
+}
+
 ## Provisions VPC peering
 resource "aws_vpc_peering_connection" "peer" {
-  count         = "${signum(var.multi_acct) + 1 % 2}"
-  auto_accept   = true
-  peer_owner_id = "${var.peer_owner_id}"
-  peer_vpc_id   = "${var.peer_vpc_id}"
-  vpc_id        = "${var.vpc_id}"
+  count = "${length(var.vpc_peering_connection_id) > 0 ? "0" : "1"}"
+
+  auto_accept   = "${length(var.accepter_owner_id) > 0 ? "false" : "true"}"
+  peer_owner_id = "${var.accepter_owner_id}"
+  peer_vpc_id   = "${var.accepter_vpc_id}"
+  vpc_id        = "${var.requester_vpc_id}"
 
   accepter {
-    allow_remote_vpc_dns_resolution = "${var.accepter_allow_remote_dns}"
+    allow_classic_link_to_remote_vpc = "${var.accepter_allow_classic_link_to_remote}"
+    allow_remote_vpc_dns_resolution  = "${var.accepter_allow_remote_dns}"
+    allow_vpc_to_remote_classic_link = "${var.accepter_allow_to_remote_classic_link}"
   }
 
   requester {
-    allow_remote_vpc_dns_resolution = "${var.requester_allow_remote_dns}"
+    allow_classic_link_to_remote_vpc = "${var.requester_allow_classic_link_to_remote}"
+    allow_remote_vpc_dns_resolution  = "${var.requester_allow_remote_dns}"
+    allow_vpc_to_remote_classic_link = "${var.requester_allow_to_remote_classic_link}"
   }
 
   tags {
-    Name        = "${var.stack_item_label}-peer"
     application = "${var.stack_item_fullname}"
     managed_by  = "terraform"
+    Name        = "${var.stack_item_label}-peer"
   }
 }
 
-resource "aws_vpc_peering_connection" "peer_multi_acct" {
-  count         = "${signum(var.multi_acct)}"
-  peer_owner_id = "${var.peer_owner_id}"
-  peer_vpc_id   = "${var.peer_vpc_id}"
-  vpc_id        = "${var.vpc_id}"
+resource "aws_vpc_peering_connection_accepter" "peer_accept" {
+  count = "${length(var.vpc_peering_connection_id) > 0 ? "1" : "0"}"
+
+  auto_accept               = "${var.accepter_auto_accept}"
+  vpc_peering_connection_id = "${var.vpc_peering_connection_id}"
 
   accepter {
-    allow_remote_vpc_dns_resolution = "${var.accepter_allow_remote_dns}"
+    allow_classic_link_to_remote_vpc = "${var.accepter_allow_classic_link_to_remote}"
+    allow_remote_vpc_dns_resolution  = "${var.accepter_allow_remote_dns}"
+    allow_vpc_to_remote_classic_link = "${var.accepter_allow_to_remote_classic_link}"
   }
 
   requester {
-    allow_remote_vpc_dns_resolution = "${var.requester_allow_remote_dns}"
+    allow_classic_link_to_remote_vpc = "${var.requester_allow_classic_link_to_remote}"
+    allow_remote_vpc_dns_resolution  = "${var.requester_allow_remote_dns}"
+    allow_vpc_to_remote_classic_link = "${var.requester_allow_to_remote_classic_link}"
   }
 
   tags {
-    Name        = "${var.stack_item_label}-peer"
     application = "${var.stack_item_fullname}"
     managed_by  = "terraform"
+    Name        = "${var.stack_item_label}-peer"
   }
 }
--- peer/variables.tf
+++ peer/variables.tf
@@ -1,36 +1,57 @@
 # Input Variables
 
 ## Resource Tags
-variable "stack_item_label" {
-  type        = "string"
-  description = "Short form identifier for this stack. This value is used to create the 'Name' resource tag for resources created by this stack item, and also serves as a unique key for re-use."
-}
-
 variable "stack_item_fullname" {
   type        = "string"
   description = "Long form descriptive name for this stack item. This value is used to create the 'application' resource tag for resources created by this stack item."
 }
 
+variable "stack_item_label" {
+  type        = "string"
+  description = "Short form identifier for this stack. This value is used to create the 'Name' resource tag for resources created by this stack item, and also serves as a unique key for re-use."
+}
+
 ## Peering parameters
+variable "accepter_allow_classic_link_to_remote" {
+  type        = "string"
+  description = "Allow a local linked EC2-Classic instance to communicate with instances in a peer VPC. This enables an outbound communication from the local ClassicLink connection to the remote VPC."
+  default     = ""
+}
+
 variable "accepter_allow_remote_dns" {
   type        = "string"
   description = "Allow accepter VPC to resolve public DNS hostnames to private IP addresses when queried from instances in the requester VPC."
+  default     = "false"
 }
 
-variable "multi_acct" {
+variable "accepter_allow_to_remote_classic_link" {
   type        = "string"
-  description = "Flag indicating whether the peering connection spans multiple AWS accounts"
-  default     = "0"
+  description = "Allow a local VPC to communicate with a linked EC2-Classic instance in a peer VPC. This enables an outbound communication from the local VPC to the remote ClassicLink connection."
+  default     = ""
 }
 
-variable "peer_owner_id" {
+variable "accepter_auto_accept" {
+  type        = "string"
+  description = "Accept the peering (both VPCs need to be in the same AWS account)."
+  default     = ""
+}
+
+variable "accepter_owner_id" {
   type        = "string"
   description = "The AWS account ID of the owner of the peer VPC."
+  default     = ""
 }
 
-variable "peer_vpc_id" {
+variable "accepter_vpc_id" {
   type        = "string"
   description = "The ID of the VPC with which you are creating the VPC Peering Connection."
+  default     = ""
+}
+
+variable "requester_allow_classic_link_to_remote" {
+  type        = "string"
+  description = "Allow a local linked EC2-Classic instance to communicate with instances in a peer VPC. This enables an outbound communication from the local ClassicLink connection to the remote VPC."
+  default     = "false"
 }
 
 variable "requester_allow_remote_dns" {
@@ -38,7 +59,20 @@ variable "requester_allow_remote_dns" {
   description = "Allow requester VPC to resolve public DNS hostnames to private IP addresses when queried from instances in the accepter VPC."
 }
 
-variable "vpc_id" {
+variable "requester_allow_to_remote_classic_link" {
+  type        = "string"
+  description = "Allow a local VPC to communicate with a linked EC2-Classic instance in a peer VPC. This enables an outbound communication from the local VPC to the remote ClassicLink connection."
+  default     = ""
+}
+
+variable "requester_vpc_id" {
   type        = "string"
   description = "The ID of the requester VPC."
+  default     = ""
+}
+
+variable "vpc_peering_connection_id" {
+  type        = "string"
+  description = "The VPC Peering Connection ID to manage."
+  default     = ""
 }
